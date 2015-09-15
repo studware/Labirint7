9f86af8c16d2f67d84e6c24739745d988ce452bd
Peter Velkov <kidroca@gmail.com>
2015-09-16 02:04:56 +0300

    Added a Visualization which should be responsible for drawing the Labyrinth and giving visual/text feedback

---
+++ src/Labyrinth-7/Visualization/Visualization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labyrinth_7.LabyrinthGrid;
+
+namespace Labyrinth_7.Visualization
+{
+    public class Visualization
+    {
+        private static Visualization singleton;
+
+        private Visualization()
+        {
+        }
+
+        public static Visualization Instance
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    singleton = new Visualization();
+                }
+
+                return singleton;
+            }
+        }
+
+        public void DrawLabyrinth(LabyrinthGrid.Labyrinth labyrinth)
+        {
+            for (int i = 0; i < labyrinth.LengthY; i++)
+            {
+                for (int j = 0; j < labyrinth.LengthX; j++)
+                {
+                    var currentGameObject = labyrinth[i, j];
+
+                    char currentSymbol = currentGameObject.Visualization;
+                    Console.BackgroundColor = currentGameObject.BackgroundColor;
+                    Console.ForegroundColor = currentGameObject.ForegroundColor;
+
+                    Console.WriteLine(currentSymbol);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
