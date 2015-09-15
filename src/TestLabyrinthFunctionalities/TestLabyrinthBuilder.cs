143d08c6832479d4985984b35f560dd4f362cbf4
Peter Velkov <kidroca@gmail.com>
2015-09-16 02:05:25 +0300

    Created a test project to test different functionality

---
+++ src/TestLabyrinthFunctionalities/TestLabyrinthBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace TestLabyrinthFunctionalities
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Labyrinth_7.LabyrinthGrid.LabyrinthGeneration;
+
+    [TestClass]
+    public class TestLabyrinthBuilder
+    {
+        [TestMethod]
+        public void Building_Small_Labyrinth_With_Default_Settings_Should_Work()
+        {
+            var smallBuilder = new SmallLabyrinthBuilder();
+            var director = new LabyrinthDirector();
+
+            director.Generate(smallBuilder);
+
+            var smallLabyrinth = smallBuilder.Labyrinth;
+        }
+    }
+}
---
+++ src/TestLabyrinthFunctionalities/TestLabyrinthVisualization.cs
@@ -0,0 +1,26 @@
+﻿namespace TestLabyrinthFunctionalities
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Labyrinth_7.LabyrinthGrid.LabyrinthGeneration;
+    using Labyrinth_7.Visualization;
+
+    [TestClass]
+    public class TestLabyrinthVisualization
+    {
+        [TestMethod]
+        public void Drawing_A_Small_Labyrinth_Should_Work()
+        {
+            var smallBuilder = new SmallLabyrinthBuilder();
+            var director = new LabyrinthDirector();
+
+            director.Generate(smallBuilder);
+
+            var smallLabyrinth = smallBuilder.Labyrinth;
+
+            var visualizator = Visualization.Instance;
+
+            visualizator.DrawLabyrinth(smallLabyrinth);
+        }
+    }
+}
