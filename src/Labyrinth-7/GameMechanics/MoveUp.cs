dcd1fa9a466572c1a03030b3b5a12e4f258db509
DimitarDKirov <dimkirov@gmail.com>
2015-10-09 23:13:40 +0300

    Added player moves classes  to implement Template Method Pattern

---
+++ src/Labyrinth-7/GameMechanics/MoveUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth_7.GameMechanics
+{
+    class MoveUp:PlayerMoves
+    {
+        protected override bool Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Labyrinth-7/GameMechanics/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth_7.GameMechanics
+{
+    abstract class PlayerMoves
+    {
+        protected abstract bool Check();
+        protected abstract void Move();
+
+        public void ExecuteCommand()
+        {
+            if(this.Check())
+            {
+                this.Move();
+            }
+        }
+    }
+}
